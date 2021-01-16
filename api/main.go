d4f04eb35b0bf14647012e49e882ba285b114373
sawady0920 <snowslidez@yahoo.co.jp>
2021-01-16 18:55:29 +0900

    add: my fisrt gql

--- api/main.go
+++ api/main.go
@@ -1,3 +1,4 @@
+/*
 package main
 
 import (
@@ -23,4 +24,35 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+}
+*/
+
+package main
+
+import (
+	"log"
+	"my-go-gql-sample/graph"
+	"my-go-gql-sample/graph/generated"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
